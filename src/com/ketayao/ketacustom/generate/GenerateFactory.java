85a7b39d3fd922f357a52153ea15ac6b129ec3f0
chen.bing <chen.bing@ampthon.com>
2015-03-02 10:17:41 +0800

    删除无用信息

--- src/com/ketayao/ketacustom/generate/GenerateFactory.java
+++ src/com/ketayao/ketacustom/generate/GenerateFactory.java
@@ -1,6 +1,5 @@
 package com.ketayao.ketacustom.generate;
 
-import com.ketayao.ketacustom.generate.GenerateSettings.SettingsType;
 import com.ketayao.ketacustom.generate.db.DataSource;
 import com.ketayao.ketacustom.generate.db.DbFactory;
 import com.ketayao.ketacustom.generate.util.ConvertHandler;
@@ -8,36 +7,36 @@ import com.ketayao.ketacustom.generate.util.FileType;
 import com.ketayao.ketacustom.generate.util.Resources;
 import com.ketayao.ketacustom.generate.vo.Table;
 
-/** 
- * @author 	<a href="mailto:ketayao@gmail.com">ketayao</a>
- * @since   2013年10月22日 下午3:53:35 
+/**
+ * @author <a href="mailto:ketayao@gmail.com">ketayao</a>
+ * @since 2013年10月22日 下午3:53:35
  */
 public class GenerateFactory {
 	private Table table;
 	private String tableName;
-	
+
 	public GenerateFactory() {
 		this.tableName = Resources.TPL_TABLE_NAME;
 	}
-	
+
 	/**
 	 * 
 	 */
 	public GenerateFactory(String tableName) {
 		this.tableName = tableName;
 	}
-	
+
 	public GenerateFactory(Table table) {
 		this.table = table;
 	}
-	
-	private void init () {
+
+	private void init() {
 		Table table = null;
 		try {
 			DataSource db = DbFactory.create();
-			
+
 			table = db.getTable(tableName);
-			
+
 			ConvertHandler.tableHandle(table);
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,7 +44,7 @@ public class GenerateFactory {
 		}
 		this.table = table;
 	}
-	
+
 	public void genJavaTemplate() {
 		try {
 			if (table == null) {
@@ -55,13 +54,13 @@ public class GenerateFactory {
 			new GenerateCode(FileType.DAO).generate(table);
 			new GenerateCode(FileType.DAOMAPPER).generate(table);
 			new GenerateCode(FileType.SERVICE).generate(table);
-//			new GenerateCode(FileType.SERVICE_IMPL).generate(table);
+			// new GenerateCode(FileType.SERVICE_IMPL).generate(table);
 			new GenerateCode(FileType.CONTROLLER).generate(table);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void genJextTemplate() {
 		try {
 			if (table == null) {
@@ -70,31 +69,12 @@ public class GenerateFactory {
 			new GenerateCode(FileType.JETX_CREATE).generate(table);
 			new GenerateCode(FileType.JETX_LIST).generate(table);
 			new GenerateCode(FileType.JETX_UPDATE).generate(table);
-			//new GenerateCode(FileType.JETX_VIEW).generate(table);
+			// new GenerateCode(FileType.JETX_VIEW).generate(table);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void genSettings() {
-		try {
-			new GenerateSettings().generate(SettingsType.APPLICATION_CONTEXT);
-			new GenerateSettings().generate(SettingsType.JDBC);
-			new GenerateSettings().generate(SettingsType.LOGBACK);
-			new GenerateSettings().generate(SettingsType.SPRING_MVC);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void copyCommons() {
-		try {
-			new CopyCommonFile().copy();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
+
 	public void genServer() {
 		try {
 			new GenerateServer().generate();
